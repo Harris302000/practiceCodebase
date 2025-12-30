60eb8ffc31af9ddd4bf415d54f740e3c87820c5d
Harris302000 <131547412+Harris302000@users.noreply.github.com>
2025-12-30 09:26:07 +0530

    Add files via upload

---
+++ javaProject/src/javaProject/main.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package javaProject;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.DoubleToIntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello world!!!");
+		
+		streamTestCases stc = new streamTestCases();
+		stc.testCases();
+	    
+		
+		 
+		
+	}
+
+}
---
+++ javaProject/src/javaProject/streamTestCases.java
@@ -0,0 +1,54 @@
+package javaProject;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class streamTestCases {
+
+	private int age;
+	
+	
+	
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public streamTestCases() {
+		
+		System.out.println("Calling Constructor");
+	}
+	
+	
+	public void testCases() {
+		
+		List<String> numbers = Arrays.asList("1", "2", "3","3","2","4");
+
+		
+		List<String> distinctList = numbers.stream().distinct().collect(Collectors.toList());
+		int sumValue = numbers.stream().distinct().mapToInt(Integer::parseInt).filter(num -> num % 2 == 0).sum();
+		System.out.println("Sumvalue::"+sumValue);
+		System.out.println("distinctList::"+distinctList);
+		
+		
+		
+		List < String > colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown", "Blue","Red");
+	    System.out.println("Original List of strings(colors): " + colors);
+	    
+	    
+	    List<String> ascOrder = colors.stream().distinct().sorted().collect(Collectors.toList());
+	    System.out.println("ascOrder::"+ascOrder);
+	    
+	    List<String> descOrder = colors.stream().distinct().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+	    System.out.println("descOrder::"+descOrder);
+		
+	}
+	
+}
