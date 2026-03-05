7de5359c7317df44f5e06997523fbbc2e3124208
Your NameHarrisFernando30 <harrisfernando30@gmail.com>
2026-03-05 22:15:49 +0530

    Spring Eclipse

---
+++ springprojecteclipse/src/main/java/com/example/springprojecteclipse/PackConfigurations.java
@@ -0,0 +1,16 @@
+package com.example.springprojecteclipse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.StudentTest.Student;
+
+@Configuration
+public class PackConfigurations {
+
+	@Bean
+	public Student student() {
+		
+		return new Student();
+	}
+}
--- springprojecteclipse/src/main/java/com/example/springprojecteclipse/SpringprojecteclipseApplication.java
+++ springprojecteclipse/src/main/java/com/example/springprojecteclipse/SpringprojecteclipseApplication.java
@@ -2,13 +2,22 @@ package com.example.springprojecteclipse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.StudentTest.Student;
 
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringprojecteclipseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringprojecteclipseApplication.class, args);
+//		SpringApplication.run(SpringprojecteclipseApplication.class, args);
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(PackConfigurations.class);
+		
+		Student student = (Student) context.getBean("student");
+		
 	}
 
 }
