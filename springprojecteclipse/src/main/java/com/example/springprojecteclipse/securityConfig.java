2d9055fe18b562897ccd79a6efc2183c85c4f8b4
Agnes Harris Fernando.A <agnes.fernando.a@newgensoft.com>
2026-06-15 18:12:49 +0530

    'ok'

--- springprojecteclipse/src/main/java/com/example/springprojecteclipse/securityConfig.java
+++ springprojecteclipse/src/main/java/com/example/springprojecteclipse/securityConfig.java
@@ -1,12 +1,23 @@
 package com.example.springprojecteclipse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class securityConfig {
 
 	@Bean
@@ -18,11 +29,44 @@ public class securityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                    .anyRequest().permitAll());
-
-        return http.build();
+		return http.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			//.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.build();
     }
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+//		UserDetails user = User.withUsername("user")
+//				.password(passwordEncoder().encode("password"))
+//				.roles("USER")
+//				.build();
+//		
+		
+		List<UserDetails> users = new ArrayList<>();
+		
+		UserDetails user1 = User.withDefaultPasswordEncoder()
+				.username("").password("").roles("USER").build();
+		
+		UserDetails user2 = User.withDefaultPasswordEncoder()
+				.username("").password("").roles("USER").build();
+		
+		UserDetails user3 = User.withDefaultPasswordEncoder()
+				.username("").password("").roles("USER").build();
+		
+		
+		users.add(user1);
+		users.add(user2);
+		users.add(user3);
+		
+		//return new InMemoryUserDetailsManager(users);
+		return new InMemoryUserDetailsManager(user1,user2,user3);
+	}
+	
+	
+	
 }
