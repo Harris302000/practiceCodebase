8bbdbc974e131b55afba903a1dba956e3d499f3e
Agnes Harris Fernando.A <agnes.fernando.a@newgensoft.com>
2026-08-03 18:02:05 +0530

    'ok'

---
+++ React-practice-code/practice-project/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return (
+
+        <footer>
+            <p>© 2026 Company Name. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ React-practice-code/practice-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    let data1 = 'Welcome, Guest';
+    return(
+        <header> 
+            <h1>{data1}</h1>    
+        </header>
+    );
+
+
+}
+
+export default Header;
