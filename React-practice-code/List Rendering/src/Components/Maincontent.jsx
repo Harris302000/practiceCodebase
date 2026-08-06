9d5b232b7e62945df33d40d3d8821ed89683df3a
Harris Fernando <harrisfernando30@gmail.com>
2026-08-06 20:47:17 +0530

    'ToDoList'

--- React-practice-code/List Rendering/src/Components/Maincontent.jsx
+++ React-practice-code/List Rendering/src/Components/Maincontent.jsx
@@ -2,11 +2,15 @@ import { useContext, useState } from "react";
 import { productContext } from "../App";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { AiTwotoneSave } from "react-icons/ai";
+
+
 
 function Main() {
   let { productlist } = useContext(productContext);
   let [prodname, setProdname] = useState("");
-  let [prodcost, setProdcost] = useState(0);
+  let [prodcost, setProdcost] = useState("");
   let [data, setData] = useState(productlist);
   let [editFlag, setEditFlag] = useState(false)
   let [curEleId, setcurEleId] = useState(null)
@@ -46,13 +50,29 @@ function Main() {
 
   const handleAddOrSave = () => {
       
+    if(prodname === ''){
+        alert('Enter the product name');
+        return;
+    }
+
+
     if(editFlag){
 
+      let newlist = data.map((item) => { return (curEleId === item.id ? {...item, name : prodname , cost : prodcost}: item ) } )
+
+      setData(newlist)
+      setcurEleId(null)
+      setProdname("")
+      setProdcost("")
+      setEditFlag(false)
+
     } else {
+
+      
       let listArr = [...data,{id : data.length + 1 , name : prodname , cost : prodcost, checked : false}]
       setData(listArr)
       setProdname("")
-      setProdcost(0)
+      setProdcost("")
     }
   }
 
@@ -80,7 +100,7 @@ function Main() {
           <input type="number" name="cost" id="cost" placeholder="Enter Cost" value={prodcost} onChange={(e) => setProdcost(e.target.value)}/>
             
           
-          <button onClick={handleAddOrSave}>{editFlag ? "Save" : "Add" }</button>
+          <button onClick={handleAddOrSave}>{editFlag ? <AiTwotoneSave />  : <IoMdAddCircleOutline /> }</button>
         </div>
 
         <table>
