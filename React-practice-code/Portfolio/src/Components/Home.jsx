279000839306555749c6df315da125ee4096e3db
Agnes Harris Fernando.A <agnes.fernando.a@newgensoft.com>
2026-08-21 16:16:48 +0530

    'Tailwindcss'

--- React-practice-code/Portfolio/src/Components/Home.jsx
+++ React-practice-code/Portfolio/src/Components/Home.jsx
@@ -5,12 +5,16 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="Home-Div">
-      <section>
-        <div style={{ width: "70%",height : '80vh',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-          <h2>Hi, I'm Agnes Harris Fernando</h2>
-          <h4>Java Full Stack Developer</h4>
-          <p style={{width : '70%',padding: '40px 0px', fontSize : '18px'}}>
+    <div className="Home-Div bg-yellow-900 min-h-screen">
+      <section className="flex-col md:flex-row">
+        <div className=" w-[90%] md:w-[70%] flex flex-col justify-center items-center h-[45vh] md:h-[80vh]" 
+        // style={{ width: "70%",height : '80vh',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}
+        >
+          <h2 className="!text-amber-300" >Hi, I'm Agnes Harris Fernando</h2>
+          <h4 className="!text-amber-300">Java Full Stack Developer</h4>
+          <p className="text-red-300 sm:text-amber-300 font-bold font-sans sd:text-[18px] w-[90%] md:w-[70%] py-[40px]" 
+          // style={{width : '70%',padding: '40px 0px'}}
+          >
             I’m a Java Developer with 4+ years of experience, primarily in the
             banking domain. I have strong experience in Core Java, Spring Boot,
             REST APIs, SQL, and microservices, and I’ve worked on Loan
@@ -31,8 +35,8 @@ const Home = () => {
           </div>
         </div>
 
-        <div style={{flex:'1'}}>
-            <img src={img1} alt="Image not found" />
+        <div className="flex-1 pt-8 md:pt-0">
+            <img src={img1} alt="Image not found" className="w-[300px]" />
           </div>
       </section>
     </div>
