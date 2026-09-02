3e4fd87e4c47463eb936f18020f826480601e7b0
Harris Fernando <harrisfernando30@gmail.com>
2026-09-02 13:06:27 +0530

    Fix Spring Boot test package

--- USER-SERVICE/src/test/java/com/example/userservice/SpringprojecteclipseApplicationTests.java
+++ USER-SERVICE/src/test/java/com/example/userservice/SpringprojecteclipseApplicationTests.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.example.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.userservice.UserServiceApplication;
+
+@SpringBootTest(classes = UserServiceApplication.class)
 class SpringprojecteclipseApplicationTests {
 
 	@Test
